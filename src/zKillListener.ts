d8b99b76868e41c4b7664c8961f1be6b2812686b
Adam Trager <we.have@unfinished.business>
2021-10-19 18:10:30 -0400

    handle zkill disconnect

--- src/zKillListener.ts
+++ src/zKillListener.ts
@@ -4,18 +4,31 @@ import WebSocket from 'ws'
 import { WormholeRegionIds } from './constants/regionIds'
 import { recordKill } from './redis'
 
-const ws = new WebSocket('wss://zkillboard.com/websocket/')
+const listen = () => {
+  const ws = new WebSocket('wss://zkillboard.com/websocket/')
 
-ws.addEventListener('open', () => {
-  WormholeRegionIds.forEach((regionId) => {
-    const payload = stringify({
-      action: 'sub',
-      channel: `region:${regionId}`,
+  ws.addEventListener('open', () => {
+    WormholeRegionIds.forEach((regionId) => {
+      const payload = stringify({
+        action: 'sub',
+        channel: `region:${regionId}`,
+      })
+
+      ws.send(payload)
     })
-
-    ws.send(payload)
+    console.info('Connected to Zkill')
   })
-  console.info('Connected to Zkill')
-})
 
-ws.addEventListener('message', recordKill)
+  ws.addEventListener('close', () => {
+    setTimeout(() => {
+      console.info('Disconnected from Zkill, reconnecting...')
+      listen()
+    }, 10000)
+  })
+
+  ws.addEventListener('error', () => ws.close())
+
+  ws.addEventListener('message', recordKill)
+}
+
+listen()
